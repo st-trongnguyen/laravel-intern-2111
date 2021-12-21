66c2704f68892d07544fb14facd695d586907525
st-trongnguyen <trong.nguyen@supremetech.vn>
2021-12-21 17:06:52 +0700

    done migration and code

--- supremetech/routes/web.php
+++ supremetech/routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
+
 use App\Http\Controllers\TaskController;
 
-=======
-use App\Http\Controllers\TestController;
->>>>>>> 5c7ea4673c35b8b4d7d923282e68457bad00a5bf
 
 /*
 |--------------------------------------------------------------------------
@@ -23,16 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
-Route::prefix('tasks')->group(function () {
-    Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
-    Route::get('/create', [TaskController::class, 'create'])->name('tasks.create');
-    Route::post('/', [TaskController::class, 'store'])->name('tasks.store');
-    Route::get('/{task}', [TaskController::class, 'show'])->name('tasks.show');
-    Route::get('/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
-    Route::patch('/{task}', [TaskController::class, 'update'])->name('tasks.update');
-    Route::delete('/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-=======
+
 Route::prefix('tasks')->name('tasks.')->group(function () {
     Route::get('/', [TaskController::class, 'index'])->name('index');
     Route::get('/create', [TaskController::class, 'create'])->name('create');
@@ -41,5 +29,4 @@ Route::prefix('tasks')->name('tasks.')->group(function () {
     Route::get('/{task}/edit', [TaskController::class, 'edit'])->name('edit');
     Route::patch('/{task}', [TaskController::class, 'update'])->name('update');
     Route::delete('/{task}', [TaskController::class, 'destroy'])->name('destroy');
->>>>>>> layout_and_module_task
 });
